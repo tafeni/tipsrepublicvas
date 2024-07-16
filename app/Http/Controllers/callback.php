5fb3beeef8188dbedfa2aa60c2fd045370279c3e
tafeni <atfsoft@gmail.com>
2024-07-16 06:10:41 +0100

    notification callback added

--- app/Http/Controllers/callback.php
+++ app/Http/Controllers/callback.php
@@ -12,7 +12,7 @@ class callback extends Controller
         // Get the content from the request
         $content = $request->getContent();
         // Append the content to the file
-        $filePath = storage_path('logs/callback_data.txt');
+        $filePath = storage_path('app/callback_data.txt');
         File::append($filePath, json_encode($content,JSON_PRETTY_PRINT) . PHP_EOL);
 
         return response()->json(['message' => 'notification received successfully.'], 200);
